7b7a23f9cc083a813d15d4c8445e333f9f8fe498
Piyush6949 <rautp6949@gmail.com>
2025-08-11 23:51:09 +0530

    Completed City Repository

--- src/repositories/cityRepository.js
+++ src/repositories/cityRepository.js
@@ -1,5 +1,58 @@
-const city = require("../models/index");
+const City = require("../models/index");
 
-async function createCity(cityName){
-    await city.create({cityName:cityName});
+class cityRepository {
+    
+    async createCity(cityName) {
+        try{
+            const newCity = await City.create({ cityName: cityName });
+            return newCity;
+        }
+        catch(error){
+            console.log("Error in city repository /n");
+            throw{error};
+        }
+    }
+
+    async deleteCity(cityId){
+        try{
+            await City.destroy({
+                where :{
+                    id : cityId
+                }
+            });
+        }
+        catch(error){
+            console.log("Error in city repository /n");
+            throw{error};
+        }
+    }
+
+    async updateCity(cityId , data) {
+        try{
+            const updatedCity = await City.update(data ,{
+                where : {
+                    id : cityId
+                }
+            });
+            return updatedCityity;
+        }
+        catch(error){
+            console.log("Error in city repository /n");
+            throw{error};
+        }
+    }
+
+    async getCity(cityId) {
+        try{
+            const city = await City.findByPk(cityId);
+            return city;
+        }
+        catch(error){
+            console.log("Error in city repository /n");
+            throw{error};
+        }
+    }
+
 }
+
+module.exports = cityRepository;
